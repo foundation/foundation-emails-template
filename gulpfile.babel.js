1a8a64367e03bde2f56d20cacf94006f9a3a595d
Zeb Pykosz <zeb.apy@gmail.com>
2016-07-08 15:06:56 -0400

    define custom data path for panini

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -51,7 +51,8 @@ function pages() {
       root: 'src/pages',
       layouts: 'src/layouts',
       partials: 'src/partials',
-      helpers: 'src/helpers'
+      helpers: 'src/helpers',
+      data: 'src/data'
     }))
     .pipe(inky())
     .pipe(gulp.dest('dist'));
