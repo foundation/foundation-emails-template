c9965f044718fe3234edca6a01998adb6bc2de27
Derek Bess <2bitsolutions@gmail.com>
2017-02-22 01:46:11 -0500

    Added Comments & Optimized Variables

--- src/helpers/ruler.js
+++ src/helpers/ruler.js
@@ -4,7 +4,7 @@
  * @example
  * {{{ruler color="#ec6225" width="60px" height="3px" spacing="15px"}}}
  * 
- * @Attribute Options
+ * Attribute Options
  * color: Must be hex color value
  * width: Can be percentage (50%) or pixel (50px) unit
  * height: Defaults to pixel unit
@@ -14,25 +14,29 @@
 
 module.exports = function(options) {
 
+	// Trim Non-Numberic Chracters
 	String.prototype.trimUnit = function() { return this.replace(/\D/g, ''); }
-	var color = options.hash.color;
-	var spacing = options.hash.spacing.trimUnit();
-	var height = options.hash.height.trimUnit();
-	var width = options.hash.width;
-	var widthType = '';
-	var trimWidth = width.trimUnit();
-	var spacer = '';
+	
+	// Variables & Options
+	var color = options.hash.color,
+	    spacing = options.hash.spacing.trimUnit(),
+	    height = options.hash.height.trimUnit(),
+	    width = options.hash.width,
+	    widthType = '',
+	    trimWidth = width.trimUnit(),
+	    spacer = '';
 
+	// Set Undefined Options
 	if (typeof color === 'undefined') color = '';
 	if (typeof height === 'undefined') height = '';
 	if (typeof width === 'undefined') width = '';
-
 	if (typeof spacing === 'undefined') {
 		spacer = '';
 	} else {
 		spacer = '<tr height="'+spacing+'"><td height="'+spacing+'"></td></tr>';
 	};
 
+	// Set Width Variables
 	if(width.match('%$')) {
 		widthType = width = trimWidth+'%';
 	} else if (width.match('px$')) {
@@ -43,6 +47,7 @@ module.exports = function(options) {
 		width = trimWidth;
 	}
 
+	// HTML Output
 	var ruler = '<table align="center" border="0" cellpadding="0" cellspacing="0" width="'+width+'" height="'+height+'" style="width:'+widthType+' !important; height: '+height+'px !important;">\
 		'+spacer+'\
 		<tr height="'+height+'"><td bgcolor="'+color+'" align="center" valign="top" width="'+width+'" height="'+height+'" style="font-size: 0%; line-height:'+height+'px; mso-line-height-rule:exactly;">\
