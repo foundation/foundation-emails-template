8d2f686022d08bc0f2eb8451a6c7552d1135d6e3
Derek Bess <2bitsolutions@gmail.com>
2017-02-22 11:33:55 -0500

    Changed code spacing
    Changed spacing from tab to spaces and size from 4 to 2.

--- src/helpers/ruler.js
+++ src/helpers/ruler.js
@@ -14,52 +14,52 @@
 
 module.exports = function(options) {
 
-	// Trim Non-Numberic Chracters
-	String.prototype.trimUnit = function() { return this.replace(/\D/g, ''); }
-	
-	// Variables & Options
-	var
+  // Trim Non-Numberic Chracters
+  String.prototype.trimUnit = function() { return this.replace(/\D/g, ''); }
+  
+  // Variables & Options
+  var
     color = options.hash.color,
-		spacing = options.hash.spacing.trimUnit(),
-		height = options.hash.height.trimUnit(),
-		width = options.hash.width,
-		widthType = '',
-		trimWidth = width.trimUnit(),
-		spacer = '';
+    spacing = options.hash.spacing.trimUnit(),
+    height = options.hash.height.trimUnit(),
+    width = options.hash.width,
+    widthType = '',
+    trimWidth = width.trimUnit(),
+    spacer = '';
 
-	// Set Undefined Options
-	if (typeof color === 'undefined') color = '';
-	if (typeof height === 'undefined') height = '';
-	if (typeof width === 'undefined') width = '';
-	if (typeof spacing === 'undefined') {
-		spacer = '';
-	} else {
-		spacer = '<tr height="'+spacing+'"><td height="'+spacing+'"></td></tr>';
-	};
+  // Set Undefined Options
+  if (typeof color === 'undefined') color = '';
+  if (typeof height === 'undefined') height = '';
+  if (typeof width === 'undefined') width = '';
+  if (typeof spacing === 'undefined') {
+    spacer = '';
+  } else {
+    spacer = '<tr height="'+spacing+'"><td height="'+spacing+'"></td></tr>';
+  };
 
-	// Set Width Variables
-	if(width.match('%$')) {
-		widthType = width = trimWidth+'%';
-	} else if (width.match('px$')) {
-		widthType = trimWidth+'px';
-		width = trimWidth;
-	} else {
-		widthType = 'auto';
-		width = trimWidth;
-	}
+  // Set Width Variables
+  if(width.match('%$')) {
+    widthType = width = trimWidth+'%';
+  } else if (width.match('px$')) {
+    widthType = trimWidth+'px';
+    width = trimWidth;
+  } else {
+    widthType = 'auto';
+    width = trimWidth;
+  }
 
-	// HTML Output
-	var ruler = '\
+  // HTML Output
+  var ruler = '\
     <table align="center" border="0" cellpadding="0" cellspacing="0" width="'+width+'" height="'+height+'" style="width:'+widthType+' !important; height: '+height+'px !important;">\
-  		'+spacer+'\
-  		<tr height="'+height+'">\
+      '+spacer+'\
+      <tr height="'+height+'">\
         <td bgcolor="'+color+'" align="center" valign="top" width="'+width+'" height="'+height+'" style="font-size: 0%; line-height:'+height+'px; mso-line-height-rule:exactly;">\
-  			 <img src="https://spacergif.org/spacer.gif" width="'+width+'" height="'+height+'" />\
-  		  </td>\
+         <img src="https://spacergif.org/spacer.gif" width="'+width+'" height="'+height+'" />\
+        </td>\
       </tr>\
-  		'+spacer+'\
-  	</table>\
+      '+spacer+'\
+    </table>\
   ';
 
-	return ruler;
+  return ruler;
 }
