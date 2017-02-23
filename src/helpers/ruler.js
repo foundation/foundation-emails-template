fd1dc0f4a350b68e137c724ef4ec6f9a7093da2e
Derek Bess <2bitsolutions@gmail.com>
2017-02-23 11:43:39 -0500

    Eliminated spacer.gif
    I managed to find a way to eliminate the need for the spacer.gif image that works in Outlook.

--- src/helpers/ruler.js
+++ src/helpers/ruler.js
@@ -2,7 +2,7 @@
  * Add a horizontal ruler to your email
  * 
  * @example
- * {{{ruler align="center" color="#ec6225" width="60px" height="3px" spacing="15px"}}}
+ * {{ruler align="center" color="#ec6225" width="60px" height="3px" spacing="15px"}}
  * 
  * Attribute Options
  * align: Can be aligned left, right or center. If Undefined alignment defaults to center.
@@ -45,9 +45,7 @@ module.exports = function(options) {
         <td>\
           <table border="0" cellpadding="0" cellspacing="0" width="100%" height="'+ spacing +'" style="width:100% !important; height: '+ spacing +'px !important;">\
             <tr>\
-              <td valign="top" height="'+ spacing +'" style="font-size: 0%; line-height: '+ spacing +'px; mso-line-height-rule:exactly;">\
-                <img src="https://spacergif.org/spacer.gif" width="100%" height="'+ spacing +'" />\
-              </td>\
+              <td valign="top" height="'+ spacing +'" style="font-size: 0%; line-height: '+ spacing +'px; mso-line-height-rule:exactly;">&nbsp;</td>\
             </tr>\
           </table>\
         </td>\
@@ -71,11 +69,9 @@ module.exports = function(options) {
       + spacer +
       '<tr>\
         <td>\
-          <table align="'+ align +'" border="0" cellpadding="0" cellspacing="0" width="'+ width +'" height="'+ height +'" style="width: '+ widthType +' !important; height: '+ height +'px !important;">\
+          <table align="'+ align +'" border="0" cellpadding="0" cellspacing="0" width="'+ width +'" style="width: '+ width +'px !important;">\
             <tr>\
-              <td bgcolor="'+ color +'" valign="top" width="'+ width +'" height="'+ height +'" style="font-size: 0%; line-height: '+ height +'px; mso-line-height-rule:exactly;">\
-                <img src="https://spacergif.org/spacer.gif" width="'+ width +'" height="'+ height +'" />\
-              </td>\
+              <td bgcolor="'+ color +'" valign="top" width="'+ width +'" height="100%" style="width: '+ width +'px !important; font-size: 0%; line-height: '+ height +'px; mso-line-height-rule:exactly;">&nbsp;</td>\
             </tr>\
           </table>\
         </td>\
